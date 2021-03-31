1082f992a74b63ed3243f13c3f94dd433f2d3ff5
iadnanmalik <61354025+iadnanmalik@users.noreply.github.com>
2021-03-31 22:32:38 +0500

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
+import axios from 'axios';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+function Reddit() {
+  const [posts, setPosts] = React.useState([]);
+
+
+React.useEffect(() => {
+  axios.get(`https://www.reddit.com/r/reactjs.json`)
+    .then(res => {
+      const newPosts = res.data.data.children
+        .map(obj => obj.data);
+
+      setPosts(newPosts);
+    });
+}, []);
+return (
+  <div>
+    <h1>/r/reactjs</h1>
+    <ul>
+      {posts.map(post => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  </div>
 );
+}
+ReactDOM.render(<Reddit />, document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*
+function Room() {
+  const [isLit, setLit] = React.useState(true);
+  const brightness = isLit ? "lit" : "dark";
+  return (
+    <div className={`room ${brightness}`}>the room is {isLit ? "lit": "dark"}
+     <br />
+    <button onClick={() => setLit(!isLit)}>
+     flip
+    </button>
+    </div>
+  );
+}
+
+ReactDOM.render(<Room />, document.getElementById('root'));
+ */
+
+
+  /*
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../src/index.css'
+function Hi(props) {
+  return <div id="id1">Hello {props.name}</div>;
+}
+
+
+const MediaCard = (props) =>{
+  return (
+ <div>
+     <h2>
+      {props.title}
+    </h2>
+    <p>
+      {props.body}
+    </p>
+    </div>
+  )
+}
+
+ReactDOM.render(<MediaCard title="Adnan" body="Computer Scientist"/>, document.querySelector('#root'));
+*/
